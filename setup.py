77c0547932f24787bdc291d10527d98bb57ef621
ZHONG KAIXIANG <xiang.ace@gmail.com>
2015-11-16 01:15:41 +0800

    [bugfix] RST
    Change-Id: I9c018bd18af3c633c2e3bb76a96ea83964bb0e21

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-
+import re
 from setuptools import setup
 
-version = '1.0.4'
+
+def find_version(fname):
+    version = ''
+    with open(fname, 'r') as fp:
+        reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+        for line in fp:
+            m = reg.match(line)
+            if m:
+                version = m.group(1)
+                break
+    if not version:
+        raise RuntimeError('Cannot find version information')
+    return version
+
+__version__ = find_version("kkconst/__init__.py")
 
 
 def read(fname):
@@ -15,29 +28,22 @@ def read(fname):
 
 setup(
     name='kkconst',
-    version=version,
-    description="Define const field and const class, customize it as You Like (Python)",
+    version=__version__,
+    description='Define const field and const class, customize it as You Like (Python)',
     long_description=read('README.rst'),
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: Implementation :: CPython",
-      ],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    keywords='kkconst',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+      ],
+    keywords=('kkconst', 'const', 'constfield'),
     author='ZHONG KAIXIANG',
     author_email='xiang.ace@gmail.com',
-    url='http://www.kaka-ace.com',
+    url='https://github.com/kaka19ace/kkconst/',
     license='http://opensource.org/licenses/MIT',
-    # packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     packages=['kkconst'],
     include_package_data=True,
-    zip_safe=True,
-    install_requires=[
-        # -*- Extra requirements: -*-
-    ],
-    entry_points="""
-    # -*- Entry points: -*-
-    """,
+    zip_safe=False
 )
