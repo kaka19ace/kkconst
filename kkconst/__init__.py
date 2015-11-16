6cfad237716c9b5c0542d326c915921e48a4bfff
ZHONG KAIXIANG <xiang.ace@gmail.com>
2015-11-16 23:49:15 +0800

    remove util, simple is better
    Change-Id: I04b3e3c431499306142902b96486851256871189

--- kkconst/__init__.py
+++ kkconst/__init__.py
@@ -9,7 +9,7 @@
 import sys
 import logging
 
-from .kkconst import (
+from .const import (
     BaseConst,
     ConstIntField,
     ConstFloatField,
@@ -19,7 +19,7 @@ from .kkconst import (
     ConstDatetimeField,
 )
 
-__version__ = "1.0.23"
+__version__ = "1.0.24a"
 
 __console_logger = None
 
